8330733472bd1ca8dea05907b033f675ba38bb61
Eduardo Patrocinio <epatro@gmail.com>
2022-01-05 16:39:41 -0300

    Implement some test  cases

--- cache/test/src/test_simple_cache.erl
+++ cache/test/src/test_simple_cache.erl
@@ -10,10 +10,20 @@ start() ->
 
 test() ->
     test_insert(),
+    test_lookup(),
     io:fwrite("Test succeeded.\n"),
     init:stop().
 
 test_insert() ->
+    io:fwrite('Testing insert.\n'),
+    ok = insert().
+
+test_lookup() ->
+    io:fwrite('Testing lookup.\n'),
+    insert(),
+    {ok,value} = simple_cache:lookup(?KEY).
+
+insert() ->
     simple_cache:insert(?KEY, ?VALUE).
 
     

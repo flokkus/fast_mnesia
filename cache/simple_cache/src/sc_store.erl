a11e7f1ec512288485da48b3183ff74392cbab80
Eduardo Patrocinio <epatro@gmail.com>
2022-01-05 13:44:34 -0300

    Simplify simple_cache

--- cache/simple_cache/src/sc_store.erl
+++ cache/simple_cache/src/sc_store.erl
@@ -10,7 +10,7 @@
 -define(TABLE_ID, ?MODULE).
 -define(WAIT_FOR_TABLES, 5000).
 
--record(key_to_pid, {key, pid}).
+-record(key_to_value, {key, value}).
 
 init() ->
     mnesia:stop(),
@@ -19,23 +19,20 @@ init() ->
     {ok, CacheNodes} = resource_discovery:fetch_resources(simple_cache),
     dynamic_db_init(lists:delete(node(), CacheNodes)).
 
-insert(Key, Pid) ->
-    mnesia:dirty_write(#key_to_pid{key = Key, pid = Pid}).
+insert(Key, Value) ->
+    mnesia:dirty_write(#key_to_value{key = Key, value = Value}).
 
 lookup(Key) ->
-    case mnesia:dirty_read(key_to_pid, Key) of
-        [{key_to_pid, Key, Pid}] ->
-	    case is_pid_alive(Pid) of
-		true -> {ok, Pid};
-		false -> {error, not_found}
-	    end;
+    case mnesia:dirty_read(key_to_value, Key) of
+        [{key_to_value, Key, Value}] ->
+	    {ok, Value};
         [] ->
 	    {error, not_found}
     end.
 
-delete(Pid) ->
-    case mnesia:dirty_index_read(key_to_pid, Pid, #key_to_pid.pid) of
-        [#key_to_pid{} = Record] ->
+delete(Key) ->
+    case mnesia:dirty_index_read(key_to_value, Key, #key_to_value.key) of
+        [#key_to_value{} = Record] ->
             mnesia:dirty_delete_object(Record);
         _ ->
             ok
@@ -45,9 +42,9 @@ delete(Pid) ->
 %% Internal Functions
 
 dynamic_db_init([]) ->
-    mnesia:create_table(key_to_pid,
-                        [{index, [pid]},
-                         {attributes, record_info(fields, key_to_pid)}
+    mnesia:create_table(key_to_value,
+                        [{index, [value]},
+                         {attributes, record_info(fields, key_to_value)}
                         ]);
 dynamic_db_init(CacheNodes) ->
     add_extra_nodes(CacheNodes).
@@ -55,7 +52,7 @@ dynamic_db_init(CacheNodes) ->
 add_extra_nodes([Node|T]) ->
     case mnesia:change_config(extra_db_nodes, [Node]) of
         {ok, [Node]} ->
-            mnesia:add_table_copy(key_to_pid, node(), ram_copies),
+            mnesia:add_table_copy(key_to_value, node(), ram_copies),
 
             Tables = mnesia:system_info(tables),
             mnesia:wait_for_tables(Tables, ?WAIT_FOR_TABLES);
@@ -63,20 +60,3 @@ add_extra_nodes([Node|T]) ->
             add_extra_nodes(T)
     end.
 
-is_pid_alive(Pid) when node(Pid) =:= node() ->
-    is_process_alive(Pid);
-is_pid_alive(Pid) ->
-    case lists:member(node(Pid), nodes()) of
-	false ->
-	    false;
-	true ->
-	    case rpc:call(node(Pid), erlang, is_process_alive, [Pid]) of
-		true ->
-		    true;
-		false ->
-		    false;
-		{badrpc, _Reason} ->
-		    false
-	    end
-    end.
-    
